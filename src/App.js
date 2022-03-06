5e9af89026afc4c2b70ca2fb8975f5470e22aa61
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-06 19:14:39 +0600

    Initial Project Setup Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Items from './pages/Items'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<Homepage />} />
+          <Route path='/items' element={<Items />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Items.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'antd/dist/antd.css'
+
+const Items = () => {
+  return (
+    <div>
+      <h1>Items Page</h1>
+    </div>
+  )
+}
+
+export default Items
