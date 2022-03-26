2a87c9da9894419a60e5a430c0a014d9ef0e88e4
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-26 23:39:57 +0600

    customer handle function added

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import DefaultLayout from '../components/DefaultLayout'
 
 const Customers = () => {
+  // handle customer render
+  const showData = () => {
+    // Code goes here
+  }
   return (
     <DefaultLayout>
       <h1>Customers Page</h1>
