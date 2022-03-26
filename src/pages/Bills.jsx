4081ef7338d25810c1461a02fa1393421626d145
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-26 23:36:51 +0600

    bill page updated

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import DefaultLayout from '../components/DefaultLayout'
 
 const Bills = () => {
+  // handle bill render
+  const showBills = () => {
+    // Code goes here
+  }
   return (
     <DefaultLayout>
       <h1>Bills Page</h1>
