efb1563c9311b2f7742ac8635c4b1105a9bb280b
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-28 00:06:30 +0600

    styling added

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -4,10 +4,12 @@ import axios from 'axios'
 import { Table, Button, Modal, Form, Input, Select, message } from 'antd'
 import DefaultLayout from '../components/DefaultLayout'
 import { EyeOutlined } from '@ant-design/icons'
+import '../assets/styles/bills.css'
 
 const Bills = () => {
   const [printBillModalVisibility, setPrintBillModalVisibility] =
     useState(false)
+  const [selectedBill, setSelectedBill] = useState(null)
 
   const dispatch = useDispatch()
   const [bills, setBills] = useState([])
@@ -54,6 +56,7 @@ const Bills = () => {
       render: (id, record) => (
         <EyeOutlined
           onClick={() => {
+            setSelectedBill(record)
             setPrintBillModalVisibility(true)
           }}
         />
@@ -68,13 +71,41 @@ const Bills = () => {
       <Table columns={columns} dataSource={bills} rowKey='_id' bordered />
       {printBillModalVisibility && (
         <Modal
+          width={800}
           onCancel={() => {
             setPrintBillModalVisibility(false)
           }}
           visible={printBillModalVisibility}
           title='Bill Details'
           footer={false}
-        ></Modal>
+        >
+          <div className='bill-model'>
+            <div className='d-flex justify-content-between bill-header pb-2'>
+              <div>
+                <h1>
+                  <b>Walmart</b>
+                </h1>
+              </div>
+              <div>
+                <p>17/09 Yangzhou</p>
+                <p>Jiangsu</p>
+                <p>China</p>
+              </div>
+            </div>
+            <div className='bill-customer-details mt-2'>
+              <p>
+                <b>Name</b> : {selectedBill.customerName}
+              </p>
+              <p>
+                <b>Phone Number</b> : {selectedBill.phoneNumber}
+              </p>
+              <p>
+                <b>Date</b> :{' '}
+                {selectedBill.createdAt.toString().substring(0, 10)}
+              </p>
+            </div>
+          </div>
+        </Modal>
       )}
     </DefaultLayout>
   )
